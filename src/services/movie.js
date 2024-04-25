0ae2ad291c2b0e7ac6f96978d6d94de00334ce9e
BeAsT436 <yuriryabenko436@gmail.com>
2024-04-25 09:35:26 +0300

    add base service

--- src/services/movie.js
+++ src/services/movie.js
@@ -1,16 +1,23 @@
 import {data} from "../data/index"
-const fetchData = async () => {
-    try {
-      const { Search, totalResults } = await new Promise((resolve) =>
-        setTimeout(() => resolve(data), 1000)
-      )
-      return {
-        Search,
-        totalResults,
-      }
-    } catch (error) {
-      console.error('Error fetching data:', error)
-      throw error
-    }
+
+class BaseService{
+  createApiUrl(params = {}){
+    const queryParams = new URLSearchParams({apikey: process.env.REACT_APP_API_KEY,...params})
+    return`${process.env.REACT_APP_BASE_URL}?${queryParams.toString()}`
   }
-export default fetchData
+}
+// const fetchData = async () => {
+//     try {
+//       const { Search, totalResults } = await new Promise((resolve) =>
+//         setTimeout(() => resolve(data), 1000)
+//       )
+//       return {
+//         Search,
+//         totalResults,
+//       }
+//     } catch (error) {
+//       console.error('Error fetching data:', error)
+//       throw error
+//     }
+//   }
+// export default fetchData
