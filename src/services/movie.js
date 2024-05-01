ac66818c8df80edc09e2a94f1df062dc28870a0f
BeAsT436 <yuriryabenko436@gmail.com>
2024-05-01 11:16:37 +0300

    add search

--- src/services/movie.js
+++ src/services/movie.js
@@ -1,23 +1,77 @@
-import {data} from "../data/index"
+import { data } from "../data/index";
 
-class BaseService{
-  createApiUrl(params = {}){
-    const queryParams = new URLSearchParams({apikey: process.env.REACT_APP_API_KEY,...params})
-    return`${process.env.REACT_APP_BASE_URL}?${queryParams.toString()}`
+class BaseService {
+  createApiUrl(params = {}) {
+    const queryParams = new URLSearchParams({
+      apikey: process.env.REACT_APP_API_KEY,
+      ...params,
+    });
+    return `${process.env.REACT_APP_BASE_URL}?${queryParams.toString()}`;
+  }
+  async simulateFetchWithDelay(data, errorChance = 0) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (Math.random() > errorChance) {
+          resolve(data);
+        } else {
+          reject(new Error("Simulated error during data retrieval"));
+        }
+      }, 5000);
+    });
+  }
+  async fetchFromApi(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    return response.json();
+    
+  }
+  
+ async fetchMoviesBySearchTerm(searchTerm, type = '', page = 1) {
+  try {
+    if (this.DEV_MODE) {
+      return await this.simulateFetchWithDelay(data)
+    } else {
+      const url = this.createApiUrl({ s: searchTerm, type, page })
+
+      return await this.fetchFromApi(url)
+    }
+  } catch (error) {
+    console.error('Error fetching movies:', error)
+    throw error
   }
 }
-// const fetchData = async () => {
-//     try {
-//       const { Search, totalResults } = await new Promise((resolve) =>
-//         setTimeout(() => resolve(data), 1000)
-//       )
-//       return {
-//         Search,
-//         totalResults,
-//       }
-//     } catch (error) {
-//       console.error('Error fetching data:', error)
-//       throw error
-//     }
-//   }
-// export default fetchData
+
+async fetchMovieById(id) {
+  try {
+    if (this.DEV_MODE) {
+      return await this.simulateFetchWithDelay(data.movie, 0.5)
+    } else {
+      const url = this.createApiUrl({ i: id })
+
+      return this.fetchFromApi(url)
+    }
+  } catch (error) {
+    console.error('Error fetching movie:', error)
+    throw error
+  }
+}
+}
+
+const fetchData = async () => {
+    try {
+      const { Search, totalResults } = await new Promise((resolve) =>
+        setTimeout(() => resolve(data), 1000)
+      )
+      return {
+        Search,
+        totalResults,
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error)
+      throw error
+    }
+  }
+export default fetchData
